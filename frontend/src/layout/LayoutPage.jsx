26abcab17f762e0cc0c14955e78d866240e2ec3f
dat <datnguyen@gmail.com>
2026-01-05 20:48:39 +0700

    set up tailwind

---
+++ frontend/src/layout/LayoutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavbarHeader from '../components/navbar/NavbarHeader'
+const LayoutPage = () => {
+  return (
+    <div className='mx-auto w-full'>
+      <div className='relative h-20 bg-red-500'>
+        <NavbarHeader/>
+      </div>
+    </div>
+  )
+}
+
+export default LayoutPage
