0aba7b9b49f0a80209472f9d237380e4a4b43027
Datdev28 <nguyenquangdat2842003@gmail.com>
2026-02-04 16:18:48 +0700

    frontend: update path for layoutPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route element={<LayoutPage />}>
+      <Route path="/" element={<LayoutPage />}>
       </Route>
       <Route path="/login" element={<LoginPage />} />
     </Routes>
