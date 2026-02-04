5ad91133bac479d35091e1666ceebcc3da8d2892
Datdev28 <nguyenquangdat2842003@gmail.com>
2026-02-04 15:17:32 +0700

    frontend: update layount and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
 import LayoutPage from "./layout/LayoutPage";
+import LoginPage from "./page/auth/LoginPage";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <LayoutPage></LayoutPage>;
+  return (
+    <Routes>
+      <Route element={<LayoutPage />}>
+      </Route>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/layout/LayoutPage.jsx
+++ frontend/src/layout/LayoutPage.jsx
@@ -10,7 +10,7 @@ const LayoutPage = () => {
       </header>
 
       {/* MAIN CONTENT */}
-      <main className="pt-16 min-h-screen bg-slate-50">
+      <main className="pt-20 min-h-screen bg-slate-50">
         <Outlet />
       </main>
     </>
