033dfe40a120e322fdd53f8f01b8ff016813c704
Datdev28 <nguyenquangdat2842003@gmail.com>
2026-02-04 16:24:09 +0700

    add route RegisterPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import LayoutPage from "./layout/LayoutPage";
 import LoginPage from "./page/auth/LoginPage";
+import RegisterPage from "./page/auth/RegisterPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <Routes>
       <Route path="/" element={<LayoutPage />}>
       </Route>
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/register" element={<RegisterPage/>}></Route>
     </Routes>
   );
 }
